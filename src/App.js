98493dc8e8de1c890bbab416e061e44f1f471afe
Vaisakhi <dasvaisakhi1@gmail.com>
2025-06-26 20:41:04 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import axios from 'axios';
+import './App.css'; 
 
 class App extends React.Component {
-    state = { advice: '' };
+    state = { advice: '', isLoading: false };
 
     componentDidMount() {
         this.fetchAdvice();
     }
 
+    playSound = () => {
+        const audio = new Audio('/sounds/click.wav'); // path from public
+        audio.play();
+    };    
+
     fetchAdvice = async () => {
+        this.playSound();
+        this.setState({ isLoading: true });
+        try {
+            const response = await axios.get('https://api.adviceslip.com/advice');
+            const advice = response.data.slip.advice;
+            this.setState({ advice, isLoading: false });
+        } catch (error) {
+            console.log('Error fetching advice:', error);
+            this.setState({ isLoading: false });
+        }
         axios.get('https://api.adviceslip.com/advice')
             .then(response => {
                 const advice = response.data.slip.advice;
@@ -20,11 +36,14 @@ class App extends React.Component {
             });
     }
     render() {
-        const { advice } = this.state;
+        const { advice, isLoading } = this.state;
         return (
             <div className='app'>
                 <div className='card'>
-                    <h1 className='heading'>{advice}</h1>
+                <h1 className={`heading ${advice ? 'fade-in' : ''}`}>{isLoading ? 'Manifesting wisdom...' : advice}</h1>
+                    <button className='button' onClick={this.fetchAdvice}>
+                        <span>Roll the wisdom dice</span>
+                    </button>
                 </div>
             </div>
         );
